763c5bcdf5fd6e6f7ebddfce521b8efd4a3baba1
developer <desenvolvedorr@hotmail.com>
2019-10-19 21:27:07 -0300

    first commit

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Administrador</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    Você esta logado como Administrador
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
